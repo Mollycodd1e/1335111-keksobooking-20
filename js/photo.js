340ef199791b9ae89b68c14dfbdfa95511f96924
Mollycodd1e <selik7@mail.ru>
2020-07-12 05:52:52 +0300

    Добавление дребезга и возможности добавления фото

---
+++ js/photo.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+    var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+    var UserPhotoChooserElement = document.querySelector('.ad-form-header__input');
+    var userPreviewElement = document.querySelector('.ad-form-header__preview img');
+    var housePhotoChooserElement = document.querySelector('.ad-form__input');
+    var housePreviewElement = document.querySelector('.ad-form__photo');
+
+    var photoChooser = function (chooser, preview) {
+        var file = chooser.files[0];
+        var fileName = file.name.toLowerCase();
+
+        var matches = FILE_TYPES.some(function (it) {
+            return fileName.endsWith(it);
+        });
+
+        if (matches) {
+            var reader = new FileReader();
+            
+            reader.addEventListener('load', function () {
+                preview.src = reader.result;
+            });
+      
+            reader.readAsDataURL(file);
+        }
+    }
+
+    UserPhotoChooserElement.addEventListener('change', function () {
+        photoChooser(UserPhotoChooserElement, userPreviewElement);
+    });
+
+    housePhotoChooserElement.addEventListener('change', function () {
+        var newHouseImageElement = document.createElement('img');
+        newHouseImageElement.width = 70;
+        newHouseImageElement.height = 70;
+        var newPreview = housePreviewElement.appendChild(newHouseImageElement);
+        console.log(newPreview)
+        photoChooser(housePhotoChooserElement, newPreview);
+    });
+})();
